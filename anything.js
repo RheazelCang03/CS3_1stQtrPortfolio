30ef3c2d115f9a4ca8be5addd57ec42bc7cf1fb1
Eyzel <b2029mrbcang@pshs.edu.ph>
2025-09-16 16:13:25 +0800

    commit 12

---
+++ anything.js
@@ -0,0 +1,15 @@
+console.log('Go Rubidium!')
+
+for(let i = 1; i <= 10; i++){
+    console.log(i);
+}
+
+grades_arr = [99, 92, 98, 100, 97]
+
+console.log(grades_arr[0]);
+
+square(3);
+
+function square(x) {
+    console.log(x * x);
+}
